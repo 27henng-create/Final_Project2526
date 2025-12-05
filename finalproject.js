4ae32e10acc6fd4e952398844f28ad940aecb576
27henng-create <27henng@badinhs.org>
2025-12-05 14:29:05 -0500

    worked on project

--- finalproject.js
+++ finalproject.js
@@ -1,2 +1,16 @@
 // This is my code for my Final Project in Programming 1
 // Hangman Game:
+    // Have a "solo" and a "multiplayer" gamemode 
+    // Solo Mode:
+        // Give computer many words for it to choose
+        // Have the computer recognize how many letters are in the word that is chosen
+        // Give the player 6 strikes if they guess wrong 6 times
+
+    // Multiplayer Mode:
+    // Let the player put in a 'secret word' and store it and have the player guess it
+
+    // OPTIONAL: Have many different themes like "video game characters", "phrases", etc.
+
+let WordChoice = ["Stove", "Guitar", "Badin High School", ""]
+
+let a = prompt("Please Enter a Letter To Guess")
