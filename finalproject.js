b47812fc13f0ee1d2b2ac0583372beb883c4cec8
27henng-create <27henng@badinhs.org>
2025-12-11 14:35:36 -0500

    worked on project

--- finalproject.js
+++ finalproject.js
@@ -16,15 +16,22 @@ let WordChoice = ["stove", "guitar", "badin high school", "stool", "egg", "metal
 let x = Math.floor(10 * Math.random());
 let playerscore = 0;
 let playerChoice;
-let playerAnswer;
+let playerAnswer = "";
 console.log(WordChoice[x]);
 //loop -> go through each letter of wordchoice and make a "-" in "playerAnswer" that corresponds to it
 
 i = 0;
 while(i < WordChoice[x].length){
+    if(WordChoice[x].charAt(i) == " "){
+        playerAnswer += " ";
+    } else {
+        playerAnswer += "-";
+    }
     console.log(WordChoice[x].charAt(i));
     i += 1;
-}
+} 
+
+console.log(playerAnswer);
 
 while(playerscore < 6){
     if(playerAnswer != WordChoice[x]){
@@ -32,14 +39,23 @@ while(playerscore < 6){
     } else if(playerAnswer == WordChoice[x]){
         console.log(`You Guessed the word! The final word was ${WordChoice}!`)
         break;
-    } 
+    }
+    
+    if (WordChoice[x].includes(playerChoice)){
+        console.log(`The string contains the letter ${playerChoice}`)
+        alert(`The string contains the letter ${playerChoice}`)
+    } else {
+        console.log(`The string does not include the letter ${playerChoice}`)
+        alert(`The string does not include the letter ${playerChoice}`)
+    }
     // have it go through each letter in the word and see if the player guesses that letter correctly
     // list out current game board aka the dashes
     // then have the computer replace the dashes with the CORRECT letters
     {
-        console.log(`The letter ${playerChoice} was not in the word.`)
-        alert(`The letter ${playerChoice} was not in the word`)
+        // console.log(`The letter ${playerChoice} was not in the word.`)
+        // alert(`The letter ${playerChoice} was not in the word`)
         playerscore += 1
     }
     playerChoice = 0
 }
+
